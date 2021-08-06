76d1d2507b783fd113813a544beaae2d875cf692
Lornus <petrenko.fpm@gmail.com>
2021-08-06 15:27:53 +0300

    deleting race

---
+++ Tests/cypress/tests/specs/races/deleting.race.test/deleting.race.test.js
@@ -0,0 +1,28 @@
+describe('Deleting race test', function () {
+    before(function () {
+        App.racePage.openUrls();
+        App.racePage.getAllLinksInTable()
+            .eq(0)
+            .click();
+    })
+
+    let text = null
+    it('Getting name of deleting race', async function () {
+        text = await App.repeatableMethods.getTextFromLocator(':nth-child(1) > em');
+
+    })
+
+    it('Click on delete button', function () {
+        cy.get('[value="Delete race"]').click();
+    })
+
+    it('After deleting planet doesn`t exist', function () {
+        App.racePage.openUrls();
+
+        App.racePage.getSearchPlaceHolder().type(text);
+        App.racePage.getFindButton().click();
+
+        expect(cy.get(App.racePage.noRaces)
+            .should('have.text', 'There no races'));
+    })
+})
